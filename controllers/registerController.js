2388bc7445b62a1589c85b68539b8c9296b4349f
RafaRdrgz <rfrdrgz@gmail.com>
2025-04-02 19:41:06 +0200

    registerrouter and registercontroller fixed

---
+++ controllers/registerController.js
@@ -0,0 +1,81 @@
+const { errorController } = require('./errorController');
+const { generateAccessToken, generateRefreshToken } = require('./tokenController');
+const { userExists, createUser } = require('../queries/userQueries');
+const { validateEmail } = require('../utils/validators');
+
+
+const registerUser = async (name, email, picture, password, google_id, auth_provider, next) => {
+
+  // Validar que el correo electrónico tenga un formato correcto
+  if (!email || !validateEmail(email)){ return errorController('Correo electrónico inválido', 400, next); }
+
+  //por defecto, la hashedPassword será null por si el auth_provider no es 'local'
+  let hashedPassword = null;
+
+
+  // Si el proveedor de autenticación esta presente y es "local", la contraseña es obligatoria
+  if (auth_provider) {
+
+    switch (auth_provider) {
+
+        case 'local':
+          // Lógica para autenticación local (con contraseña)
+          if (!password) {
+            return errorController('La contraseña es requerida para autenticación local', 400, next);
+          }
+          //lógica para crear un usuario con contraseña
+          hashedPassword = await bcrypt.hash(password, 10);
+          break;
+      
+        case 'google':
+          // Lógica para autenticación con Google (sin contraseña)
+          if (!google_id) {
+            return errorController('Se requiere google_id para autenticación Google', 400, next);
+          }
+          // Aquí puedes agregar la lógica para crear un usuario con Google (sin contraseña)
+          /* DE MOMENTO NO TOCAR */
+          break;
+      
+        default:
+          // Lógica por defecto (para autenticación por otros medios, si se desea)
+          return errorController('Proveedor de autenticación no soportado', 400, next);
+      }
+
+
+  } else {
+
+    return errorController('No auth_provider', 400, next);
+  }
+
+  try{
+
+    // Verificar si el usuario ya existe en la base de datos
+    const exists = await userExists(email);
+
+    if (exists) { return errorController('El correo ya está registrado', 400, next); }
+
+    // Insertar el nuevo usuario en la base de datos
+    const newUser = await createUser(name, email, picture, hashedPassword, google_id, auth_provider);
+
+    // Generar los tokens
+    const accessToken = await generateAccessToken(newUser, next);
+    const refreshToken = await generateRefreshToken(newUser, next);
+
+    // Devolver la respuesta con el usuario y los tokens
+    return { message: 'Usuario registrado con éxito', newUser, accessToken, refreshToken };
+
+
+  } catch(error){
+    console.error('Error registrando usuario:', error);
+    return errorController('Error en el servidor', 500, next);
+  }
+
+}
+
+
+
+module.exports = {
+
+    registerUser
+
+}
--- routes/RegisterRouter.js
+++ routes/RegisterRouter.js
@@ -1,60 +1,25 @@
 const express = require('express');
 const { errorController } = require('../controllers/errorController');
-const { generateAccessToken, generateRefreshToken } = require('../controllers/tokenController');
-const { userExists, createUser } = require('../queries/userQueries');
-const { validateEmail } = require('../utils/validators');
+const { registerUser } = require('../controllers/registerController');
 
 const RegisterRouter = express.Router(); // Inicializamos el router
 
 // Crear un nuevo usuario (registro)
-RegisterRouter.post('/register', async (req, res) => {
+RegisterRouter.post('/register', async (req, res,next) => {
 
   const { name, email, picture, password, google_id, auth_provider } = req.body;
 
-  // Validar que el correo electrónico tenga un formato correcto
-  if (!email || !validateEmail(email)){ return errorController('Correo electrónico inválido', 400, next); }
+ try{
 
-  // Verificar si la contraseña está presente
-  // Si el proveedor de autenticación es "local", la contraseña es obligatoria
-  if (auth_provider === 'local' && !password) {
+      return await registerUser(name, email, picture, password, google_id, auth_provider,next);
 
-    return errorController('La contraseña es requerida para autenticación local', 400, next);
+ } catch(error){
 
-  }
+  console.error('Error registrando usuario:', error);
+  return errorController('Error en el servidor', 500, next);
 
-  try {
+ }
 
-    // Verificar si el usuario ya existe en la base de datos
-    const exists = await userExists(email);
-
-    if (exists) { return errorController('El correo ya está registrado', 400, next); }
-
-    //por defecto, la hashedPassword será null por si el auth_provider no es 'local'
-    let hashedPassword = null;
-
-    // Si el proveedor es local, ciframos la contraseña
-    if (auth_provider === 'local') {
-      
-      hashedPassword = await bcrypt.hash(password, 10);
-
-    }
-
-        // Insertar el nuevo usuario en la base de datos
-    const newUser = await createUser(name, email, picture, hashedPassword, google_id, auth_provider);
-
-    // Generar los tokens
-    const accessToken = await generateAccessToken(newUser, next);
-    const refreshToken = await generateRefreshToken(newUser, next);
-
-    // Devolver la respuesta con el usuario y los tokens
-    return res.status(201).json({ message: 'Usuario registrado con éxito', newUser, accessToken, refreshToken });
-
-  } catch (error) {
-
-    console.error('Error registrando usuario:', error);
-    return errorController('Error en el servidor', 500, next);
-    
-  }
 });
 
 
